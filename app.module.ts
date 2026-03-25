b81b290f00a85c7cbace4d1a20e896990eabf4c5
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-03-25 20:46:14 +0600

    Remove ConfigModule import from app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionsModule } from './transactions/transactions.module';
