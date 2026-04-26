f61d7c2a7437d0cfe911b7e118d919f3bc3e4561
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-04-26 15:21:58 +0600

    Fix CORS configuration for frontend URL

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3001", 
+    origin: process.env.FRONTEND_URL ?? 
     credentials: true, 
   });
 
