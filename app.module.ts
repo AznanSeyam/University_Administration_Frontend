01fd8a28e7994b32c209c0a7267f8aecd959fabc
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-05-04 10:25:07 +0600

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
