31ecc071c2bdd57fe78e3161c508053ca32b325b
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-05-12 22:49:24 +0600

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -7,7 +7,6 @@ import { Module } from '@nestjs/common';
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT, 10),
       username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASS,
       database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
